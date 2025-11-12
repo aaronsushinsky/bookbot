5f0f210703880524a552eca3c67bbf403471153d
aaronsushinsky <aaronsushinsky@gmail.com>
2025-11-11 21:06:20 -0600

    E: added sorted dictionary for letter count

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
-from stats import get_word_count
+from stats import get_word_count, get_character_count, get_sorted_letters
 
 def get_book_text(filepath):
     with open(filepath) as f:
         words = f.read()
-        total = get_word_count(words)
-    return total
+        total_words = get_word_count(words)
+    return total_words
 
 def main ():
     num_words = get_book_text("books/frankenstein.txt")
-    print(f'Found {num_words} total words')
+    num_letters = get_character_count("books/frankenstein.txt")
+    sorted_letters = get_sorted_letters(num_letters)
 
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f'Found {num_words} total words')
+    print("--------- Character Count -------")
+    for line in sorted_letters:
+        checker = line["char"]
+        if checker.isalpha() == True:
+            print(f'{line["char"]}: {line["num"]}')
+        else:
+            pass
+    print("============= END ===============")
 main()    
--- stats.py
+++ stats.py
@@ -1,3 +1,30 @@
 
 def get_word_count(text):
     return len(text.split())
+
+def get_character_count(text):
+    with open(text) as f:
+        words = f.read()
+        letter_count = {}
+        for letter in words:
+            letter = str.lower(letter)
+            if letter in letter_count:
+                newnum = letter_count[letter]
+                letter_count[letter] = newnum + 1
+            elif letter not in letter_count:
+                letter_count[letter] = 1     
+        return letter_count
+
+def get_sorted_letters(dictionary):
+    sorted_dictionary = []
+    for key in dictionary:
+        mini_dict = {}
+        mini_dict["char"] = key
+        mini_dict["num"] = dictionary[key]    
+        sorted_dictionary.append(mini_dict)
+        sorted_dictionary.sort(reverse=True, key=sort_function)
+    return sorted_dictionary
+
+
+def sort_function(new_list):
+    return new_list["num"]
