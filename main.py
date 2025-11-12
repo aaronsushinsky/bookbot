ddb916e879af74a63df0434daccc21d9203d8ee5
aaronsushinsky <aaronsushinsky@gmail.com>
2025-11-11 19:35:30 -0600

    D: shortened function, was causing errors

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
+from stats import get_word_count
+
 def get_book_text(filepath):
     with open(filepath) as f:
         words = f.read()
         total = get_word_count(words)
     return total
 
-def get_word_count(text):
-    new_words = text.split()
-    counter = 0
-    for word in new_words:
-        counter += 1
-    return counter
-    
 def main ():
     num_words = get_book_text("books/frankenstein.txt")
     print(f'Found {num_words} total words')
---
+++ stats.py
@@ -0,0 +1,3 @@
+
+def get_word_count(text):
+    return len(text.split())
