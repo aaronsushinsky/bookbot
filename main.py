1ae46ad7283a036ca48f888c078d7fa0edfb16b9
aaronsushinsky <aaronsushinsky@gmail.com>
2025-11-11 19:04:38 -0600

    C: seperated word count function

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 def get_book_text(filepath):
     with open(filepath) as f:
         words = f.read()
-        new_words = words.split()
-        counter = 0
-        for word in new_words:
-            #print(word, counter)
-            counter += 1
-    return counter
+        total = get_word_count(words)
+    return total
 
+def get_word_count(text):
+    new_words = text.split()
+    counter = 0
+    for word in new_words:
+        counter += 1
+    return counter
+    
 def main ():
     num_words = get_book_text("books/frankenstein.txt")
     print(f'Found {num_words} total words')
-
-
 
 main()    
